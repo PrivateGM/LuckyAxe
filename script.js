ec0a90f3a3295c445c46ae4526d4c6c9434e3dc2
PrivateGM <yourdeath651@gmail.com>
2024-09-26 22:23:54 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
-document.getElementById("username").innerText = `Привет, ${first_name}!`;
+document.getElementById("username").innerText = 'Привет, ${first_name}!';
+//document.getElementById("username").innerText = "1";
